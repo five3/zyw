57cb5768c34a40b661d0902c5a0c706aed1c699b
Xiaowu Chen <five3@163.com>
2015-12-23 14:20:43 +0800

    Update config.py

--- zhiyuw/config.py
+++ zhiyuw/config.py
@@ -2,6 +2,8 @@
 __author__ = 'macy'
 
 settings = {'SITE_TITLE': "职语网", 'logincode':False, 'site_domain' : 'www.zhiyuw.com'}
-position_imgs = [{'url':'#','src':'/static/uploadfiles/image/20150526/thumb_29d39884345728ff2f7a5bfff4f773f1.jpg'},
-                     {'url':'#','src':'/static/uploadfiles/image/20150526/thumb_44022d985ec7307b73c00daf18668b32.jpg'},
-                     {'url':'#','src':'/static/uploadfiles/image/20151221/thumb_44022d985ec7307b73c00daf18668b33.jpg'}]
+position_imgs = [{'url':'#','src':'/static/uploadfiles/image/20151223/banner01.jpg'},
+                     {'url':'#','src':'/static/uploadfiles/image/20151223/banner02.jpg'},
+                     {'url':'#','src':'/static/uploadfiles/image/20151223/banner03.jpg'},
+                     {'url':'#','src':'/static/uploadfiles/image/20151223/banner04.jpg'},
+                     {'url':'#','src':'/static/uploadfiles/image/20151223/banner05.jpg'}]
